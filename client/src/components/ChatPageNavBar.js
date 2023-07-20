2ae19c5d706d0be768d799b53e3e4ecee17bb230
totimehjames123 <99030266+totimehjames123@users.noreply.github.com>
2023-07-20 14:27:14 +0000

    Update ChatPageNavBar.js

--- client/src/components/ChatPageNavBar.js
+++ client/src/components/ChatPageNavBar.js
@@ -4,13 +4,9 @@ import { NavLink } from 'react-router-dom'
 function ChatPageNavBar({id}) {
   return (
     <div className='bg-gray-50 flex items-center' style={{width: '100%', height: '10vh'}}>
-    <div className='rounded m-3 bg-white' style={{width: '98%', height: '90%', marginTop: '20px', borderRadius: '20px', paddingLeft: '20px'}}>
-      <img src='logo.jpeg' width={50} height={50} alt='logo'/>
-    </div>
-      {/* <span>{id}</span> */}
-      {/* <NavLink to="/mainpage">Chat</NavLink>
-      <NavLink to="/about">About</NavLink>
-      <NavLink to="/contact">contact</NavLink> */}
+      <div className='rounded m-3 bg-white' style={{width: '98%', height: '90%', marginTop: '20px', borderRadius: '20px', paddingLeft: '20px'}}>
+        <img src='logo.jpeg' width={50} height={50} alt='logo'/>
+      </div>
     </div>
   )
 }
